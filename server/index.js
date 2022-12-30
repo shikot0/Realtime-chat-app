ba8f0f73ceccfe625087c7dbb20fb9b3d0a386a6
shikoto <1sh1m1da1@gmail.com>
2022-12-30 02:01:02 +0000

    fixed socket

--- server/index.js
+++ server/index.js
@@ -29,7 +29,8 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        // origin: 'http://localhost:3000',
+        origin: 'reca.vercel.app',
         credentials: true,
     },
 });
